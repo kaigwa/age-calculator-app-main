4f69a82fb73997ba1fa6dfaaebdd9f2f6a300347
Reset <kaigwaedwin@gmail.com>
2023-06-20 15:29:38 +0300

    New Features

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // call variables
 const input = document.querySelectorAll('input');
 const button = document.getElementById('btn');
-const displayYear = document.getElementById('year');
-const displayMonth = document.getElementById('month');
-const displayDay = document.getElementById('day');
+
+const displayAge = document.querySelectorAll('main p span');
+
+const errorAlert = document.querySelectorAll('.required')
+const errorP = document.querySelectorAll('.d')
 
 // getting date
 const todayDate = new Date();
@@ -13,10 +15,8 @@ let todayYear = todayDate.getFullYear();
 
 const numberOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-// console.log(todayDate)
 
 // call function
-
 const calculateAge = () => {
     let y = todayYear - input[2].value
     let m = todayMonth - input[1].value
@@ -35,9 +35,77 @@ const calculateAge = () => {
     day.textContent = d
     month.textContent = m
     year.textContent = y
+
+    inputValidate()
+}
+
+
+// handle validation
+const inputValidate = () =>{
+    const dayvalue = input[0].value
+    const monthvalue = input[1].value
+    const yearvalue = input[2].value
+
+    // day validation section
+    if(!dayvalue){
+        errorAlert[0].textContent = 'This field is required';
+        input[0].style.border = '0.9px solid hsl(0, 100%, 67%)';
+        errorP[0].style.color = 'hsl(0, 100%, 67%)';
+        displayAge.forEach(item =>item.textContent = '--' )
+    } 
+    else if(dayvalue > numberOfMonth[monthvalue - 1]|| 
+        dayvalue < 1){
+        errorAlert[0].textContent = 'Must be a valid date';
+        input[0].style.border = '0.9px solid hsl(0, 100%, 67%)';
+        errorP[0].style.color = 'hsl(0, 100%, 67%)';
+        displayAge.forEach(item =>item.textContent = '--' )
+    } 
+    else {
+        errorAlert[0].textContent = ''
+        input[0].style.border = '0.9px solid hsl(0, 0%, 86%)';
+        errorP[0].style.color = 'hsl(0, 1%, 44%)';
+    }
+
+    // month validation section;
+    if(!monthvalue){
+        errorAlert[1].textContent = 'This field is required';
+        input[1].style.border = '0.9px solid hsl(0, 100%, 67%)';
+        errorP[1].style.color = 'hsl(0, 100%, 67%)';
+        displayAge.forEach(item =>item.textContent = '--' )
+    } 
+    else if(monthvalue > 12|| monthvalue < 1){
+        errorAlert[1].textContent = 'Must be a valid month';
+        input[1].style.border = '0.9px solid hsl(0, 100%, 67%)';
+        errorP[1].style.color = 'hsl(0, 100%, 67%)';
+        displayAge.forEach(item =>item.textContent = '--' )
+    }
+    else {
+        errorAlert[1].textContent = ''
+        input[1].style.border = '0.9px solid hsl(0, 0%, 86%)';
+        errorP[1].style.color = 'hsl(0, 1%, 44%)';
+    }
+
+    // year validation section
+    if(!yearvalue){
+        errorAlert[2].textContent = 'This field is required';
+        input[2].style.border = '0.9px solid hsl(0, 100%, 67%)';
+        errorP[2].style.color = 'hsl(0, 100%, 67%)';
+        displayAge.forEach(item =>item.textContent = '--' )
+
+    } 
+    else if(yearvalue > todayYear|| 
+        new Date (yearvalue,monthvalue - 1, dayvalue) > todayDate){
+        errorAlert[2].textContent = 'Must be in the past';
+        input[2].style.border = '0.9px solid hsl(0, 100%, 67%)';
+        errorP[2].style.color = 'hsl(0, 100%, 67%)';
+        displayAge.forEach(item =>item.textContent = '--' )
+    }
+    else {
+        errorAlert[2].textContent = ''
+        input[2].style.border = '0.9px solid hsl(0, 0%, 86%)';
+        errorP[2].style.color = 'hsl(0, 1%, 44%)';
+    }
 }
 
 // add event listener
 button.addEventListener('click', calculateAge)
-
-// handle validation
